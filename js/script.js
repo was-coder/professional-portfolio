23be2ea02134901c8748316b658a85fbeceb95b4
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-11-06 16:10:39 +0100

    feat: add the Javascript code and format the code with stylelint

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-let mobileMenu = document.getElementById("hamburger-div");
-let mobileMenuCancel = document.getElementById("cancel-hamburger");
-mobileMenu.addEventListener("click", ()=>{document.querySelector(".mobile-div").classList.add('show')});
-mobileMenuCancel.addEventListener("click", ()=>{document.querySelector(".mobile-div").classList.remove('show')});
+let mobileMenu = document.getElementById('hamburger-div');
+let mobileMenuCancel = document.getElementById('cancel-hamburger');
+mobileMenu.addEventListener('click', () => {
+  document.querySelector('.mobile-div').classList.add('show');
+});
+mobileMenuCancel.addEventListener('click', () => {
+  document.querySelector('.mobile-div').classList.remove('show');
+});
