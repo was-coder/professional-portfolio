8ab8e5633a107cb4a5a81ecab35c92b89f15f37c
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-11-06 17:13:18 +0100

    feat: Update Javascript code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let mobileMenu = document.getElementById('hamburger-div');
 let mobileMenuCancel = document.getElementById('cancel-hamburger');
+let mobileMenuCancel1 = document.getElementById('cancel-butt1');
+let mobileMenuCancel2 = document.getElementById('cancel-butt2');
+let mobileMenuCancel3 = document.getElementById('cancel-butt3');
 mobileMenu.addEventListener('click', () => {
   document.querySelector('.mobile-div').classList.add('show');
 });
 mobileMenuCancel.addEventListener('click', () => {
+  document.querySelector('.mobile-div').classList.remove('show');
+});
+mobileMenuCancel1.addEventListener('click', () => {
+  document.querySelector('.mobile-div').classList.remove('show');
+});
+mobileMenuCancel2.addEventListener('click', () => {
+  document.querySelector('.mobile-div').classList.remove('show');
+});
+mobileMenuCancel3.addEventListener('click', () => {
   document.querySelector('.mobile-div').classList.remove('show');
 });
