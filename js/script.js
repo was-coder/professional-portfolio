d329e3c6da5e8413a70967d4f63883b138c61758
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-01-29 22:35:49 +0100

    fix:Add the eslint report to the linter file and also remove the preventdefault

--- js/script.js
+++ js/script.js
@@ -13,3 +13,42 @@ const removeActiveClass = () => {
 cancelMobile.forEach(x => {
   x.addEventListener('click', removeActiveClass);
 });
+
+// start of email validation
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+function setError(value, message) {
+  const formControl = value.parentElement;
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+  formControl.className = 'form-control getError';
+}
+
+function setSuccess(value, message) {
+  const formControl = value.parentElement;
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+  formControl.className = 'form-control getSuccess';
+}
+
+function emailCheck(email) {
+  const getEmail = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return getEmail.test(String(email).toLowerCase());
+}
+
+function getValues() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === '') {
+    setError(email, 'Email cannot be blank');
+  } else if (!emailCheck(emailValue)) {
+    setError(email, 'Email is not valid');
+  } else {
+    setSuccess(email, 'Input Accepted');
+  }
+}
+
+form.addEventListener('click', () => {
+  getValues();
+});
